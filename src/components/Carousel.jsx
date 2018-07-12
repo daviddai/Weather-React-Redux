1fdd090c6d7dfed8c0d82e28b8eaf285f242fbac
ddai <ddai@tripadvisor.com>
2018-07-12 13:16:07 +1000

    Refactored carousel and added slide component

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import Slider from "react-slick";
 
+import Slide from "./Slide";
+
+import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
+
 
 class Carousel extends React.Component {
 
@@ -10,15 +15,20 @@ class Carousel extends React.Component {
 
     render() {
         const settings = {
+            dots: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 1,
+            slidesToScroll: 1,
             arrows: false
         };
 
         return (
             <div className="col-12">
                 <Slider {...settings}>
-                    <div>
-                        <h1>1</h1>
-                    </div>
+                    <Slide/>
+                    <Slide/>
+                    <Slide/>
                 </Slider>
             </div>
         );
---
+++ src/components/Slide.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+class Slide extends React.Component {
+
+    render() {
+        return (
+            <div className="card">
+                <div className="card-body">
+                    <div className="text-center">
+                        Monday
+                    </div>
+                </div>
+                <div className="card-footer">
+                    <div className="text-center">
+                        Weather
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+}
+
+export default Slide;
