a7bca4849edc1558d5eb3c33e586383cd818ad38
ddai <ddai@tripadvisor.com>
2018-07-12 17:12:19 +1000

    added images and refactor component slide

--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -1,13 +1,25 @@
 import React from "react";
+import SunRain from "../images/sun_cloud_rain.png";
 
 class Slide extends React.Component {
 
     render() {
+        const imageStyle = {
+            width: "100%"
+        };
+
         return (
-            <div className="card">
+            <div className="card border-0">
                 <div className="card-body">
                     <div className="text-center">
-                        Monday
+                        <h1 className="align-content-center font-weight-bold display-3">Sydney</h1>
+                        <p className="pt-2">Partly cloudy with possible rain</p>
+                    </div>
+                    <div>
+                        <img src={SunRain} style={imageStyle} />
+                    </div>
+                    <div className="text-center">
+                        <h3 className="align-content-center font-weight-bold">15</h3>
                     </div>
                 </div>
                 <div className="card-footer">
