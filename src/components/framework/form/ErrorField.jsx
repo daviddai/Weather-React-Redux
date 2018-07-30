ce5f002ec7945f192554371d594b5d46cd491098
ddai <ddai@tripadvisor.com>
2018-07-30 16:13:17 +1000

    error field wip

--- src/components/framework/form/ErrorField.jsx
+++ src/components/framework/form/ErrorField.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 
+import PropTypes from "prop-types";
+
 class ErrorField extends React.Component {
 
     render() {
         return (
             <div className={this.props.classNames}>
                 <span className={this.props.iconClassNames}/>
-                abc
+                {this.props.errorMessage}
             </div>
         );
     }
 
 }
+
+ErrorField.propTypes = {
+    classNames: PropTypes.string,
+    iconClassNames: PropTypes.string,
+    errorMessage: PropTypes.string
+};
 
 export default ErrorField;
